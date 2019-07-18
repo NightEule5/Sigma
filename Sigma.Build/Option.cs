1e599b3ac4603210508459cebb6d6e1252af01b0
NightEule5 <wyatt_andersen@hotmail.com>
2019-07-18 11:40:47 -0600

    Cleanup and Option stuff

--- Sigma.Build/Option.cs
+++ Sigma.Build/Option.cs
@@ -1,5 +1,18 @@
-﻿// Copyright (c) 2019 Phillip Leere (NightEule5)
-// Subject to MIT License (found in LICENSE.md).
+﻿/*
+ * Copyright 2019 Phillip Leere
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
 using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
@@ -32,6 +45,32 @@ namespace Sigma
         }
 
         /// <summary>
+        /// Initializes a new instance of <see cref="Option"/> with the specified default
+        /// value and type.
+        /// </summary>
+        /// <param name="type">The type; must match <paramref name="defaultValue"/>'s type.</param>
+        /// <param name="defaultValue">The default value; type must match <paramref name="type"/>.</param>
+        public Option(Type type, object defaultValue) : this()
+            => Default = (Type = type) == defaultValue.GetType() ? defaultValue
+            : throw new ArgumentException("The type of defaultValue does not " +
+                $"match the Type given ({type.Name}).", "defaultValue");
+
+        public Option(bool value) : this(typeof(bool), value) { }
+        public Option(byte value) : this(typeof(byte), value) { }
+        public Option(char value) : this(typeof(char), value) { }
+        public Option(decimal value) : this(typeof(decimal), value) { }
+        public Option(double value) : this(typeof(double), value) { }
+        public Option(float value) : this(typeof(float), value) { }
+        public Option(int value) : this(typeof(int), value) { }
+        public Option(long value) : this(typeof(long), value) { }
+        public Option(sbyte value) : this(typeof(sbyte), value) { }
+        public Option(short value) : this(typeof(short), value) { }
+        public Option(string value) : this(typeof(string), value) { }
+        public Option(uint value) : this(typeof(uint), value) { }
+        public Option(ulong value) : this(typeof(ulong), value) { }
+        public Option(ushort value) : this(typeof(ushort), value) { }
+
+        /// <summary>
         /// Creates a new <see cref="Option"/> with the specified default value and type.
         /// </summary>
         /// <typeparam name="T">The option type, inferred from usage.</typeparam>
@@ -39,7 +78,8 @@ namespace Sigma
         /// <returns></returns>
         public static Option Create<T>(T defaultValue) => new Option()
         {
-            Default = defaultValue, Type = typeof(T)
+            Default = defaultValue,
+            Type = typeof(T)
         };
     }
 }
